00acc0bc700598b3806a3c34f52f802cf3649444
whzh <whzh@Mac.local>
2014-05-30 20:26:45 +0800

    init the first version

---
+++ index.js
@@ -0,0 +1,14 @@
+var toc = require('marked-toc');
+
+module.exports = {
+  book: {
+    assets: "./book"
+  },
+  hooks: {
+    "page:before": function(page) {
+      var tmpl = '<%= depth %><%= bullet %>[<%= heading %>](#<%= url %>)\n';
+      page.content = toc.insert(page.content, {template: tmpl});
+      return page;
+    }
+  }
+};
