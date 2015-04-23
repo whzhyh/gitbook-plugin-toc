8a8cfa5b7eddfea984bf08675e5671132497d132
Willin Wang <willin@willin.org>
2015-04-23 23:30:17 +0800

    add ClassName optional

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-var toc = require('markdown-toc-gitbook');
-const template = require('./node_modules/marked-toc/node_modules/template');
+var toc = require('markdown-toc');
 
 module.exports = {
-  book: {
-  },
-  hooks: {
-    "page:before": function(page) {
-      var tmpl = '<%= depth %><%= bullet %>[<% var h=depth.length/2+1; if (typeof(h1) == "undefined") {h1=0;} if (typeof(h2) == "undefined") {h2=0;} if (typeof(h3) == "undefined") {h3=0;} var head; if (h == 1) {h1++;h2=0;h3=0;head=h1;} else if (h == 2) {h2++;h3=0;head=h1+"."+h2;} else if (h == 3) {h3++;head=h1+"."+h2+"."+h3;} %><%= head%> <%= heading %>](#<%= url %>)\n';
-      page.content = toc.insert(page.content, {template: tmpl});
-      template('<% h1=0 %>', {});
-      return page;
-    }
-  }
+	book: {},
+	hooks: {
+		"page:before": function (page) {
+			page.content = toc.insert(page.content, {
+				slugify: function (str) {
+					return encodeURI(str.toLowerCase()).replace(/%20/g, '-');
+				}
+			});
+			console.log(this.options);
+			if (this.options.pluginsConfig.toc.addClass) {
+				var className = this.options.pluginsConfig.toc.className || 'toc';
+				page.content = page.content + '\n\n\n<script type="text/javascript">var targetUl = document.getElementsByClassName(\'page-inner\')[0].getElementsByTagName(\'ul\')[0];if(targetUl.getElementsByTagName(\'a\').length>0){targetUl.className=\'' + className + '\';}</script>';
+			}
+			return page;
+		}
+	}
 };
---
+++ node_modules/markdown-toc/lib/insert.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var matter = require('gray-matter');
+var toc = require('..');
+
+/**
+ * The basic idea:
+ *
+ *  1. when front-matter exists, we need to avoid turning its properties into headings.
+ *  2. We need to detect toc markers on the page. For now it's a simple HTML code comment
+ *     to ensure the markdown is compatible with any parser.
+ *
+ * @param  {String} `str` Pass a string of markdown
+ * @param  {Object} `options` Pass options to toc generation
+ * @return {String} Get the same string back with a TOC inserted
+ */
+
+module.exports = function insert(str, options) {
+  options = options || {};
+
+  var regex = options.regex || /(?:<!-- toc(?:\s*stop)? -->)/g;
+  var open = options.open   || '<!-- toc -->\n\n';
+  var close = options.close || '<!-- tocstop -->';
+  var file;
+
+  // does the file have front-matter?
+  if (/^---/.test(str)) {
+    // extract it temporarily so the syntax
+    // doesn't get mistaken for a heading
+    file = matter(str);
+    str = file.content;
+  }
+
+  var sections = split(str, regex);
+  if (sections.length > 3) {
+    throw new Error('markdown-toc only supports one Table of Contents per file.');
+  }
+
+  var last = sections[sections.length - 1];
+  if (sections.length === 3) {
+    sections.splice(1, 1, open + (options.toc || toc(last, options).content));
+    sections.splice(2, 0, close);
+  }
+
+  if (sections.length === 2) {
+    sections.splice(1, 0, open + toc(last, options).content + '\n\n' + close);
+  }
+
+  var res = sections.join('\n\n');
+  // if front-matter was found, put it back now
+  if (file) {
+    return matter.stringify(res, file.data);
+  }
+  return res;
+};
+
+function split(str, re) {
+  return str.split(re).map(trim);
+}
+
+function trim(str) {
+  return str.trim();
+}
